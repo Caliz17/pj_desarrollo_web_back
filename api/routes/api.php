cc0c633125e91cae0098477c7452daa58da77fe8
Isaac Caliz <isaaccaliz305@gmail.com>
2024-09-01 21:13:28 -0600

    ✨ Feat: inicio api

--- api/routes/api.php
+++ api/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// mostarar un mensaje de bienvenida y que diga api version 1.0 en el inicio
+Route::get('/', function () {
+    return response()->json(['message' => 'API version 1.0']);
+});
